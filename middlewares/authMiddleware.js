6b60ed664f8bf2bb23eff9e39f13a8062729b4c6
Arsalan-4797 <arsalanhabib4378@gmail.com>
2024-10-10 17:37:22 +0500

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,1 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateJWT = (req, res, next) => {
+    const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Access Denied' });
+    }
+
+    jwt.verify(token, 'your_secret_key', (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ message: 'Invalid Token' });
+        }
+
+        req.userId = decoded.userId;
+        next();
+    });
+};
+
+module.exports = authenticateJWT;
 
